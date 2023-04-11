5a043eb9217384c3faaad4179da2b1bc648e15bb
tom-backend <tom@xcsholdings.xyz>
2023-04-12 04:10:17 +0900

    test

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.get('/simsimi', async function (req, res) {
     })
   res.send(response);
 });
-app.listen(443, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+module.exports = app;
